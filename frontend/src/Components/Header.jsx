85b25eec8f9fa05f026700037b8cb245ead89dc5
Hamza FELLAH <fellahhamza48@gmail.com>
2024-05-24 18:54:34 +0100

    Modifing the header

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,28 +1,14 @@
 import React from "react";
-import UserProfile from "./UserProfile";
 import EMSPI from "../images/EMSPI.png";
 
 const Header = () => {
   return (
-    <>
-      {/* component */}
-      <nav className="relative">
-        {/* Sky blue color block */}
-        <div className="bg-sky-800 w-full h-12"></div>
-
-        {/* Logo */}
-        <div className="absolute left-0 top-0 flex items-center ml-4">
-          <img src={EMSPI} alt="Logo" className="w-24 h-auto" />
-          <p className="text-white text-2xl font-semibold tracking-wide">
-            {" "}
-            Engineering Modular System Project Information
-          </p>
-        </div>
-
-        {/* Black color block */}
-        <div className="bg-slate-700 w-full h-12"></div>
-      </nav>
-    </>
+    <nav className="bg-sky-800 w-full h-17 flex items-center px-4 ">
+      <img src={EMSPI} alt="Logo" className="w-20 h-auto mr-4" />
+      <p className="text-white text-xl font-semibold tracking-wide">
+        JESA - Engineering Modular System Project Information
+      </p>
+    </nav>
   );
 };
 
--- frontend/src/Components/userSideBar.jsx
+++ frontend/src/Components/userSideBar.jsx
@@ -5,8 +5,8 @@ import LogoutButton from "./LogoutButton";
 export default function userSideBar() {
   return (
     <>
-      <aside className="flex flex-col w-64 h-screen px-5 py-8 overflow-y-auto bg-white border-r rtl:border-r-0 rtl:border-l dark:bg-gray-900 dark:border-gray-700 bg-slate-200">
-        <div className="flex flex-col justify-between flex-1 mt-6">
+      <aside className="flex flex-col w-64 h-screen px-5 py-7 overflow-y-auto bg-white border-r rtl:border-r-0 rtl:border-l dark:bg-gray-900 dark:border-gray-700 bg-slate-200">
+        <div className="flex flex-col justify-between flex-1">
           <nav className="-mx-3 space-y-6 ">
             <div className="space-y-3 ">
               <label className="px-3 text-xs text-gray-500 uppercase dark:text-gray-400">
@@ -172,7 +172,7 @@ export default function userSideBar() {
                 Readers
               </label>
               <Link
-                className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
+                className=" mt-4 mb-3 flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
                 to="/user/WordFile"
               >
                 <svg
@@ -211,7 +211,7 @@ export default function userSideBar() {
           <label className="text-xs text-gray-500 uppercase dark:text-gray-400">
             Account
           </label>
-          <div>
+          <div className="mt-2">
             <LogoutButton />
           </div>
         </div>
